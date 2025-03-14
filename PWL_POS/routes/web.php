64d49659f27fcb0f94c9fa8595ae87930aca4272
“JihaR15 <jiharamdhan97@gmail.com>
2025-03-14 15:35:11 +0700

    Update Implementasi J5 ke PWL_POS dan Edit Readme.md

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -31,3 +31,12 @@ Route::get('/user/ubah/{id}',[UserController::class,'ubah']);
 Route::put('/user/ubah_simpan/{id}',[UserController::class,'ubah_simpan']);
 
 Route::get('/user/hapus/{id}',[UserController::class,'hapus']);
+// J5 Praktikum 2
+Route::get('kategori',[KategoriController::class,'index'])->name('kategori.index');
+// J5 Praktikum 3
+Route::get('kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
+Route::post('/kategori', [KategoriController::class, 'store']);
+
+Route::get('kategori/edit/{id}', [KategoriController::class, 'edit'])->name('kategori.edit');
+Route::post('kategori/edit', [KategoriController::class, 'update']);
+Route::get('kategori/hapus/{id}', [KategoriController::class, 'delete']);
