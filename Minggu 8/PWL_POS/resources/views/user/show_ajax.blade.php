0f66911de0e9c0c23c69f2ee82fdcef649faf121
“JihaR15 <jiharamdhan97@gmail.com>
2025-04-05 06:45:02 +0700

    Add: J8 Tugas 4 (Implementasi Upload File Gambar)

--- Minggu 8/PWL_POS/resources/views/user/show_ajax.blade.php
+++ Minggu 8/PWL_POS/resources/views/user/show_ajax.blade.php
@@ -7,27 +7,36 @@
             </button>
         </div>
         <div class="modal-body">
-            <table class="table table-bordered table-striped table-hover table-sm">
-                <tr>
-                    <th>ID</th>
-                    <td>{{ $user->user_id }}</td>
-                </tr>
-                <tr>
-                    <th>Level</th>
-                    <td>{{ $user->level->level_nama }}</td>
-                </tr>
-                <tr>
-                    <th>Username</th>
-                    <td>{{ $user->username }}</td>
-                </tr>
-                <tr>
-                    <th>Nama</th>
-                    <td>{{ $user->nama }}</td>
-                </tr>
-                <tr>
-                    <th>Password</th>
-                    <td>********</td>
-                </tr>
-            </table>
+            <div class="d-flex align-items-start align-items-center">
+                <div class="mr-4">
+                    <img src="{{ $user->picture_path ?? asset('profile_placeholder.png') }}" alt="Foto Profil" class="rounded-circle"
+                        style="width: 150px; height: 150px; object-fit: cover;">
+                </div>
+
+                <div class="flex-grow-1">
+                    <table class="table table-bordered table-striped table-hover table-sm">
+                        <tr>
+                            <th class="text-nowrap" style="width: 90px;">ID</th>
+                            <td>{{ $user->user_id }}</td>
+                        </tr>
+                        <tr>
+                            <th class="text-nowrap">Level</th>
+                            <td>{{ $user->level->level_nama }}</td>
+                        </tr>
+                        <tr>
+                            <th class="text-nowrap">Username</th>
+                            <td>{{ $user->username }}</td>
+                        </tr>
+                        <tr>
+                            <th class="text-nowrap">Nama</th>
+                            <td>{{ $user->nama }}</td>
+                        </tr>
+                        <tr>
+                            <th class="text-nowrap">Password</th>
+                            <td>********</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
     </div>
 </div>
